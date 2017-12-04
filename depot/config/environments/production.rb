6129d6e2c5765b38169a7016ef79203f356b82f7
abhishekkanojia <abhishek.kanojia@vinsol.com>
2017-12-04 16:44:34 +0530

    Iteration H1 completed

--- depot/config/environments/production.rb
+++ depot/config/environments/production.rb
@@ -57,6 +57,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "depot_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "mail.gmail.com",
+    authentication: "plain",
+    user_name: "dujam2014",
+    password: "ceo@2014",
+    enable_starttls_auto: true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
