88da5b99808ba53e749d8aaee89d6a1bb17eb9cb
abhishekkanojia <abhishek.kanojia@vinsol.com>
2017-12-08 19:01:14 +0530

    started Fixes according to review

--- depot/db/migrate/20171208102526_add_columns_to_products.rb
+++ depot/db/migrate/20171208102526_add_columns_to_products.rb
@@ -1,7 +1,9 @@
 class AddColumnsToProducts < ActiveRecord::Migration[5.0]
   def change
-    add_column :products, :enabled, :boolean
-    add_column :products, :discount_price, :decimal
-    add_column :products, :permalink, :string
+    change_table :products do |t|
+      t.boolean :enabled
+      t.string :permalink
+      t.decimal :discount_price
+    end
   end
 end
--- depot/db/migrate/20171208110127_change_default_value_of_enabled.rb
+++ depot/db/migrate/20171208110127_change_default_value_of_enabled.rb
@@ -1,5 +1,7 @@
 class ChangeDefaultValueOfEnabled < ActiveRecord::Migration[5.0]
   def change
-    change_column_default :products, :enabled, false
+    change_column_default :products do |t|
+      t.boolean :enabled, false
+    end
   end
 end
