ab121aa72edcbbaac524d04071910126b4dc4d78
abhishekkanojia <abhishek.kanojia@vinsol.com>
2017-12-11 12:24:36 +0530

    Added bulk true option to migration add_columns_to_products for faster migration

--- depot/db/migrate/20171208102526_add_columns_to_products.rb
+++ depot/db/migrate/20171208102526_add_columns_to_products.rb
@@ -1,6 +1,6 @@
 class AddColumnsToProducts < ActiveRecord::Migration[5.0]
   def change
-    change_table :products do |t|
+    change_table :products, bulk: true do |t|
       t.boolean :enabled
       t.string :permalink
       t.decimal :discount_price
