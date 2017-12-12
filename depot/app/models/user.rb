31e9d3c0e42702180e1d4d3d8bb958ed8a26b74c
abhishekkanojia <abhishek.kanojia@vinsol.com>
2017-12-12 18:42:12 +0530

    Removed unneccessary spaces and dots

--- depot/app/models/user.rb
+++ depot/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :name, presence: true, uniqueness: true
-  validates :email, uniqueness: true, format: { with: EMAIL_REGEX, multiline: true. case_sensitive: false }
+  validates :email, uniqueness: true, format: { with: EMAIL_REGEX, multiline: true, case_sensitive: false }
   has_secure_password
 
   after_destroy :ensure_an_admin_remains
