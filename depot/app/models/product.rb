c9e41e3c342b2d34c816521737f4aed9c04cdf28
abhishekkanojia <abhishek.kanojia@vinsol.com>
2017-12-13 12:47:09 +0530

    Removed unnecessary spaces

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -6,7 +6,6 @@ class Product < ApplicationRecord
   has_many :orders, through: :line_items
   before_destroy :ensure_not_referenced_by_any_line_item
 
-
   validates :title, presence: true, uniqueness: true
   validates :image_url, presence: true, allow_blank: true, image_url: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }, allow_nil: true
@@ -29,7 +28,6 @@ class Product < ApplicationRecord
     too_long: 'Must have atmost 10 words'
   }
   # validates :discount_price, with: :validate_price_greater_than_discount
-
 
   private
 
